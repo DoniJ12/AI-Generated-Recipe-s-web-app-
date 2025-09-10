d8e8b2d5283b24f771ce26fc54b43da4d018158f
Doni J <donijosy67@gmail.com>
2025-09-10 21:45:27 +0300

    Added interactivity to add ingredients

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,14 +1,34 @@
-export default function () {
-  return (
-    <main>
-      <form className="add-ingredient-form">
-        <input
-          aria-label="main form"
-          type="text"
-          placeholder="e.g. Shiro"
-        />
-        <button>Add Ingridient</button>
-      </form>
-    </main>
-  )
+import React from "react"
+
+export default function Main() {
+
+    const [ingredients, setIngredients] = React.useState([])
+
+    const ingredientsListItems = ingredients.map(ingredient => (
+        <li key={ingredient}>{ingredient}</li>
+    ))
+
+    function handleSubmit(event) {
+        event.preventDefault()
+        const formData = new FormData(event.currentTarget)
+        const newIngredient = formData.get("ingredient")
+        setIngredients(prevIngredients => [...prevIngredients, newIngredient])
+    }
+
+    return (
+        <main>
+            <form onSubmit={handleSubmit} className="add-ingredient-form">
+                <input
+                    type="text"
+                    placeholder="e.g. oregano"
+                    aria-label="Add ingredient"
+                    name="ingredient"
+                />
+                <button>Add ingredient</button>
+            </form>
+            <ul>
+                {ingredientsListItems}
+            </ul>
+        </main>
+    )
 }
