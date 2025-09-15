08f88345e78690b31d257a55105f26fe43f0e6ad
Doni J <donijosy67@gmail.com>
2025-09-15 15:06:56 +0300

    Added some styles to the AI response

--- src/components/ClaudeRecipe.jsx
+++ src/components/ClaudeRecipe.jsx
@@ -1,8 +1,9 @@
+import ReactMarkdown from "react-markdown";
+
 export default function (props) {
   return (
-    <section>
-      <h2>Chef Claude Recommends:</h2>
-      {props.recipe}
+    <section className="suggested-recipe-container">
+      <ReactMarkdown>{props.recipe}</ReactMarkdown>
     </section>
   );
 }
